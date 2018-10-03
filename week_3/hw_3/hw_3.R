ec7636dc456beb240b0a4c7f05cd4e057f8ee623
張靖雍 <goodjob0823@gmail.com>
2018-10-03 21:44:05 +0800

    data visualization

--- week_3/hw_3/hw_3.R
+++ week_3/hw_3/hw_3.R
@@ -24,10 +24,6 @@ my_data_organic <- my_data[grep("organic", my_data$type), ]
 grep("organic", my_data$type)
 View(my_data_organic)
 
-my_data_Boston <- my_data[grep("Boston", my_data$region), ]
-grep("Boston", my_data$region)
-View(my_data_Boston)
-
 my_data_Atlanta <- my_data[grep("Atlanta", my_data$region), ]
 grep("Atlanta", my_data$region)
 View(my_data_Atlanta)
@@ -52,117 +48,207 @@ my_data_composite <- bind_rows(my_data_Atlanta, my_data_Boston, my_data_Californ
 ggplot(data = my_data_composite, mapping = aes(x = region, y = AveragePrice/338)) + geom_bar(stat = "identity", color = "grey")
 
 
+library(ggmap)
+library(mapproj)
+register_google <- function(key = "AIzaSyBBeUPDxrLXly4GA0FoSucx0b6fhl4byHI", day_limit = 1000)
+register_google
 
 
 
-
-------------------------------------------------------------
-
-
-
-
-help(geom)
-
-
-
-
-
-data_type <- type[,"conventional"]
-my_data_1 <- my_data[,'type']
-
-
-
-
-
-
-
-total_volume <- matrix(round(runif(14)*18249), nrow = 14)  
-  
-
-
-
-
-
-
-
-
-
-
-filter(type == "conventional" & region == "Albany") %>%
-
-my_data_2 <- select(my_data, Total.Volume, type)
-my_data_2
-
-newdata <- my_data[which(my_data$type=="conventional")]
-newdata
-
-
-
-my_data_a <- my_data[ ,"type"]
-my_data_a
-my_data %% group_by(type) %% summarise(Total.Volume)
-my_data %>% group_by(type) %%
-  summarise(Total.Volume = sum(row = Total.Volume), type = "conventional")
-
-sum(subset(my_data, type == "conventional", select == "Total.Volume"))
-select(my_data, contains("conventional"))
-select(my_data, contains("organic"))
-
-
-class(my_data)
-summary(my_data)
-
-
-
-
-
-
-
-install.packages("hexbin")
-install.packages("dplyr")
-getwd()
-
-
-
-
-
-
-
-
-
-
-
+install.packages(mapproj)
+library(mapproj)
+library(forcats)
 
 install.packages("ggmap")
-install.packages("mapproj")
-
-library(ggmap)
-library(mapproj)
-
-map <- get_googlemap(location = 'Canada', zoom = 4)
-ggmap(map)
-
-
-library(ggmap)
-library(mapproj)
-map <- get_map( location =  c(lon = 105.3632715, lat = 29.7632836),zoom = 7)
-ggmap(map)
-
-
-get_map()
 
 
 
-library(ggmap)
-library(mapproj)
-map <- get_map( location =  c(lon = 70.3632715, lat = 65.7632836),zoom = 7)
-print(ggmap(map))
 
 
 
-# 問題：
-如何搜集特定的數據？(ex. 只想搜集conventional，但寫完之後一直說"type not found")
-如何畫出多個變數的ggplot圖
-如何把atlanta、boston等多個表合併？並重新弄一個region?因為原來的表有50多個region，圖塞不下......
-如何查詢region是否有new york?
 
+
+
+register_google <- function (AIzaSyBBeUPDxrLXly4GA0FoSucx0b6fhl4byHI, account_type, client, signature, second_limit, day_limit) {
+  
+  # get current options
+  options <- getOption("ggmap")
+  
+  # check for client/sig specs
+  if (!missing(client) &&  missing(signature) ) {
+    stop("if client is specified, signature must be also.")
+  }
+  if ( missing(client) && !missing(signature) ) {
+    stop("if signature is specified, client must be also.")
+  }
+  if (!missing(client) && !missing(signature) ) {
+    if (goog_account() == "standard" && missing(account_type)) {
+      stop("if providing client and signature, the account type must be premium.")
+    }
+  }
+  
+  # construct new ones
+  if(!missing(key)) options$google$key <- key
+  if(!missing(account_type)) options$google$account_type <- account_type
+  if(!missing(day_limit)) options$google$day_limit <- day_limit
+  if(!missing(second_limit)) options$google$second_limit <- second_limit
+  if(!missing(client)) options$google$client <- client
+  if(!missing(signature)) options$google$signature <- signature
+  
+  # # set premium defaults
+  if (!missing(account_type) && account_type == "premium") {
+    if(missing(day_limit)) options$google$day_limit <- 100000
+  }
+  
+  # class
+  class(options) <- "ggmap_credentials"
+  
+  # set new options
+  options(ggmap = options)
+  
+  # return
+  invisible(NULL)
+}
+
+
+
+
+
+
+
+#' @rdname register_google
+#' @export
+goog_key <- function () {
+  
+  getOption("ggmap")$google$key
+  
+}
+
+#' @rdname register_google
+#' @export
+has_goog_key <- function () {
+  
+  if(is.null(getOption("ggmap"))) return(FALSE)
+  
+  !is.na(goog_key())
+  
+}
+
+
+
+
+
+
+
+#' @rdname register_google
+#' @export
+has_goog_account <- function () {
+  
+  if(is.null(getOption("ggmap"))) return(FALSE)
+  
+  !is.na(goog_account())
+  
+}
+
+#' @rdname register_google
+#' @export
+goog_account <- function () {
+  
+  getOption("ggmap")$google$account_type
+  
+}
+
+
+
+
+
+
+
+
+#' @rdname register_google
+#' @export
+goog_client <- function () {
+  
+  getOption("ggmap")$google$client
+  
+}
+
+#' @rdname register_google
+#' @export
+has_goog_client <- function () {
+  
+  if(is.null(getOption("ggmap"))) return(FALSE)
+  
+  !is.na(goog_client())
+  
+}
+
+
+
+
+
+
+
+
+#' @rdname register_google
+#' @export
+goog_signature <- function () {
+  
+  getOption("ggmap")$google$signature
+  
+}
+
+#' @rdname register_google
+#' @export
+has_goog_signature <- function () {
+  
+  if(is.null(getOption("ggmap"))) return(FALSE)
+  
+  !is.na(goog_signature())
+  
+}
+
+
+
+
+
+#' @rdname register_google
+#' @export
+goog_second_limit <- function () {
+  
+  # set to 50 if no key present (ggmap not loaded)
+  if(!has_goog_key()) return(50)
+  
+  getOption("ggmap")$google$second_limit
+  
+}
+
+
+
+#' @rdname register_google
+#' @export
+goog_day_limit <- function () {
+  
+  # set to 2500 if no key present (ggmap not loaded)
+  if(!has_goog_key()) return(2500)
+  
+  getOption("ggmap")$google$day_limit
+  
+}
+
+
+
+
+
+
+
+#' @rdname register_google
+#' @param tree a json tree from \code{\link{fromJSON}}
+#' @export
+check_google_for_error <- function (tree) {
+  
+  if (tree$status == "REQUEST_DENIED") {
+    warning("REQUEST_DENIED : ", tree$error_message)
+  }
+  
+}
